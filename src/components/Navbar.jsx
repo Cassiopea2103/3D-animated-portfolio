bef95649fe98e7c54b4d9908b67bf9b685344847
unknown <wadess@ept.sn>
2024-08-03 07:43:57 +0000

    Implemented Navbar feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import { logo , menu , close } from '../assets' ;
 
 const Navbar = () => {
     const [ active , setActive ] = useState ()
+    const [ toggle , setToggle ] = useState ( false ) ; 
 
     return (
         <nav className={`${ styles.paddingX } w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -54,6 +55,51 @@ const Navbar = () => {
                         ))
                     }
                 </ul>
+
+                {/* navlinks for mobile view : */}
+                <div className='sm:hidden flex flex-1 justify-end items-center'>
+                    <img 
+                        src = { toggle ? close : menu }
+                        alt='Menu button' 
+                        className='w-[28px] h-[28px] object-contain cursor-pointer'
+                        onClick = { () => setToggle ( !toggle  ) } 
+                    />
+
+                    <div className={`${
+                        !toggle 
+                        ? 'hidden' 
+                        : 'flex'
+                        }
+                        p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl
+                    `}>
+
+                        <ul className='list-none flex flex-col justify-end items-start gap-5'>
+                            {
+                                navLinks.map ( ( link ) => (
+                                    <li
+                                        key={link.id}
+                                        className={`${
+                                            active === link.title 
+                                            ? "text-white"  
+                                            : "text-secondary"
+                                            }
+                                            font-poppins font-medium text-[16px]  cursor-pointer
+                                        `}
+                                        onClick = { () => {
+                                            setActive ( link.title ) ; 
+                                            setToggle ( !toggle ) ; 
+                                        }}
+                                    >
+                                        <a href={`#${ link.id }`}>
+                                            { link.title }
+                                        </a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+
+                    </div>
+                </div>
             </div>
 
         </nav>
